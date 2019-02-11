78ade3762842f7f7ef01312ac96c40237e62d694
CharlesDiGI <charles.digiampietro@gmail.com>
2019-02-11 21:41:15 +0100

    refonte template mail pour new user et new event

--- app/mailers/event_mailer.rb
+++ app/mailers/event_mailer.rb
@@ -4,21 +4,13 @@ class EventMailer < ApplicationMailer
   def new_event_email(event)
     #on récupère l'instance user pour ensuite pouvoir la passer à la view en @user
     @event = event 
-    @admin = event.admin_id 
-    @user = User.find(@admin)
-    puts "*" *30
-    puts @event.inspect
-    puts "*" *30
-    puts @admin.inspect
-    puts "*" *30
-    puts @user.email
-    puts "*" *30
+    @admin = event.admin
 
     #on définit une variable @url qu'on utilisera dans la view d’e-mail
-    @url  = 'http://monsite.fr/login' 
+    @url  = 'http://hackbrite.fr/login' 
 
     # c'est cet appel à mail() qui permet d'envoyer l’e-mail en définissant destinataire et sujet.
-    mail(to: @user.email, subject: 'Nouvel évènement bien créé !') 
+    mail(to: @admin.email, subject: 'Nouvel évènement bien créé !') 
   end
 
 end
