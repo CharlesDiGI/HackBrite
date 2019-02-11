1f43745bc5b38cf69140683fb6e5eb5435ffc915
CharlesDiGI <charles.digiampietro@gmail.com>
2019-02-11 12:01:31 +0100

    création des tables avec liaisons

--- app/models/event.rb
+++ app/models/event.rb
@@ -2,8 +2,8 @@ class Event < ApplicationRecord
 
   has_many :attendances
 
-  has_many :attendants, class_name: "User" #, foreign_key: :attendant_id
-  has_many :attendants, through: :attendance
+  has_many :attendees, class_name: "User" #, foreign_key: :attendee_id
+  has_many :attendees, through: :attendance
 
-  belongs_to :admin, class_name: "User" #, foreign_key: :user_id
+  belongs_to :admin, class_name: "User" #, foreign_key: :admin_id
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ApplicationRecord
 
 has_many :events, class_name: 'Event', foreign_key: 'admin_id'
-has_many :events, class_name: 'Event', foreign_key: 'attendant_id'
+has_many :events, class_name: 'Event', foreign_key: 'attendee_id'
 has_many :events, through: :attendance
 
 
