116fcb3eadce74ad1e274b1d5ea0c21dad68bf42
CharlesDiGI <charles.digiampietro@gmail.com>
2019-02-11 16:39:07 +0100

    seed créé

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,19 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+#remise a zero des id dans toutes les tables
+ActiveRecord::Base.connection.tables.each do |t|
+  ActiveRecord::Base.connection.reset_pk_sequence!(t)
+end
+
+
+#Create User
+10.times do
+  user = User.create!(first_name: Faker::DragonBall.character, last_name: Faker::Name.last_name , description: Faker::HowIMetYourMother.quote, email: Faker::Internet.email)
+end
+
+#Create Event
+5.times do
+  event = Event.create!(title: Faker::Dessert.variety, description: Faker::ChuckNorris.fact, start_date: Faker::Date.between(Time.now, 1.week.from_now), duration: 5 * (1 + rand(60)), price: Faker::Number.between(1, 1000), location: Faker::Address.city, attendee_id: User.all.ids.sample, admin_id: User.all.ids.sample)
+end
