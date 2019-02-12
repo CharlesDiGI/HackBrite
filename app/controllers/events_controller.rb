c1847e1686358f6a327f4e69820264e3a33fb961
CharlesDiGI <charles.digiampietro@gmail.com>
2019-02-12 22:45:59 +0100

    views pour les users et crééation d'event

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,5 +1,30 @@
 class EventsController < ApplicationController
+  
   def index
     @events = Event.all
   end
+
+  def show
+    @event = Event.find(params[:id])
+  end
+  
+  def new
+    @event = Event.new
+  end
+  
+  def create
+    @event = Event.create(title: param[:title], description: param[:description], price: param[:price],
+      location: param[:location], duration: param[:duration], start_date: param[:start_date], admin: current_user)
+
+    if @event.save
+      flash[:notice] = "Post successfully created by #{current_user.first_name}"
+      flash[:type] = "info"
+      redirect_to root_path
+    else
+      render 'new'
+    end
+
+  end
+  
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,12 @@
 Rails.application.routes.draw do
 
+  get 'users/show'
+  get 'users/edit'
+  get 'users/update'
+  get 'users/destroy'
   devise_for :users
   resources :events, :path => '/'
+  resources :users
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
