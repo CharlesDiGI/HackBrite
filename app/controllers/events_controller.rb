cad2e31a2650f7a2753c8f5668505bc877489306
CharlesDiGI <charles.digiampietro@gmail.com>
2019-02-13 22:37:34 +0100

    index event edit and destroy OK

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,5 +1,6 @@
 class EventsController < ApplicationController
   before_action :authenticate_user!, only: [:new]
+  before_action :valid_user, only: [:destroy, :update, :edit]
   
   def index
     @events = Event.all
@@ -31,6 +32,52 @@ class EventsController < ApplicationController
       end
     end
   end
+
+  def edit
+    @event = Event.find(params[:id])
+  end
+
+  def update
+    @event = Event.find(params[:id])
+    respond_to do |format|
+      if @event.update(event_params)
+        format.html { redirect_to @event, notice: 'Event was successfully updated.' }
+        format.json { render :show, status: :ok, location: @event }
+      else
+        format.html { render :edit }
+        format.json { render json: @event.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  def destroy
+    @event = Event.find(params[:id])
+    if current_user == @event.admin
+      @event.destroy
+      flash[:notice] = "Post successfully deleted"
+      flash[:type] = "danger"
+      redirect_to user_path(current_user)
+    else
+      flash[:notice] = "You're not the admin of this event"
+      flash[:type] = "danger"
+      redirect_to event_path(@event)
+    end
+    # respond_to do |format|
+    #   format.html { redirect_to events_url, notice: 'Event was successfully destroyed.' }
+    #   format.json { head :no_content }
+    # end
+  end
+    # @event = Event.find(params[:id])
+    # post_params = params[:event]
+
+    # if @event.update(start_date: post_params[:start_date], title: post_params[:title], description: post_params[:description], price: [:location, duration: post_params[:duration]])
+  	# 	redirect_to user_path(params[:id])
+  	# else
+  	#   flash[:danger] = "Il manque des informations"
+  	#   render :new 
+  	# end
+
+
 
     # @event = Event.create(title: param[:title], description: param[:description], price: param[:price],
     #   location: param[:location], duration: param[:duration], start_date: param[:start_date], admin: current_user)
