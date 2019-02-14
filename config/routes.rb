26db4db7b38d260ec022ff3e1c804a46532d9350
CharlesDiGI <charles.digiampietro@gmail.com>
2019-02-14 23:24:32 +0100

    ajout de l'avatar pour l'user

--- config/routes.rb
+++ config/routes.rb
@@ -2,10 +2,14 @@ Rails.application.routes.draw do
   root 'events#index'
   devise_for :users
   # resources :events, :path => '/'
-  resources :users
+  resources :users do
+    resources :avatars, only: [:create]
+  end
+  
   resources :events do
     resources :attendances
     resources :charges, only: [:new, :create]
+    resources :event_pics, only: [:create]
   end
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
