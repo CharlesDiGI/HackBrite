257fc17a98719749ffe85276463674e36a2dad6a
CharlesDiGI <charles.digiampietro@gmail.com>
2019-02-15 15:54:13 +0100

    création table liaison events submission

---
+++ spec/factories/event_statuses.rb
@@ -0,0 +1,7 @@
+FactoryBot.define do
+  factory :event_status do
+    pending { false }
+    accepted { false }
+    refused { false }
+  end
+end
---
+++ spec/factories/event_submissions.rb
@@ -0,0 +1,5 @@
+FactoryBot.define do
+  factory :event_submission do
+    
+  end
+end
